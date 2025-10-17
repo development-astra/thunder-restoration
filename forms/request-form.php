9ef4f5cd5c2586fc88fe9973070c4779a064d565
carvelruss <russserafin15@gmail.com>
2025-10-17 10:16:59 +0800

    Fixed SMTP

--- forms/request-form.php
+++ forms/request-form.php
@@ -24,8 +24,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'harry@astraresults.com';
-        $mail->Password   = 'pzuhhxmmirtxsjry';
+        $mail->Username   = 'thunderrleads@gmail.com';
+        $mail->Password   = 'zflhesrnuqbeqncc';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
 
